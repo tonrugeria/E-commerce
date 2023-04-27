97de0e199f5b15efd78c7c2c8d833458e274ead8
TON RUGERIA <tonrugeria@gmail.com>
2023-04-28 00:25:16 +0800

    NavBar and Outlet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 import Home from "./components/routes/home/home.component";
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 };
