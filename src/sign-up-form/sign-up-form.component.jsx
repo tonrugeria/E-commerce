d683c66e80956f9e2b796e863d5e291b23617479
TON RUGERIA <tonrugeria@gmail.com>
2023-04-30 22:12:44 +0800

    Sign up with email and password

--- src/sign-up-form/sign-up-form.component.jsx
+++ src/sign-up-form/sign-up-form.component.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../utils/firebase/firebase.utils"
 
 const defaultFormFields = {
   displayName: '',
@@ -13,6 +14,31 @@ const SignUpForm = () => {
 
   console.log(formFields);
 
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields)
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    if (password !== confirmPassword) {
+      alert("passwords do not match")
+      return
+    }
+
+    try {
+      const { user } = await createAuthUserWithEmailAndPassword(email, password)
+      await createUserDocumentFromAuth(user, {displayName })
+      resetFormFields()
+    } catch (error) {
+      if(error.code === "auth/email-already-in-use") {
+        alert("Cannot create user/email already in use")
+      } else {
+        console.log("user creation encounter an error", error );
+      }
+    }
+  }
+
   const handleChange = (event) => {
     const {name, value} = event.target
 
@@ -22,7 +48,7 @@ const SignUpForm = () => {
   return (
     <div>
       <h1>Sign up with your email and password</h1>
-      <form onSubmit={() => {}}>
+      <form onSubmit={handleSubmit}>
         <label>Display Name</label>
         <input type="text" required onChange={handleChange} name="displayName" value={displayName}/>
 
